3d35f7d4e3fd9625a7bc4dd7ec279fb6786f5e15
ismaildedec <ddcismail@gmail.com>
2025-02-03 22:28:42 +0300

    I created LoadingSpinner component with conditional processing

---
+++ components/config/common/LoadingSpinner.js
@@ -0,0 +1,6 @@
+import styles from '../../../styles/Config.module.scss';
+export const LoadingSpinner = () => (
+    <div className={styles.spinnerContainer}>
+        <div className={styles.spinner}></div>
+    </div>
+);
